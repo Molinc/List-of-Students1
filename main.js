798d1949927b0a624bd934a616de33048f4dfac2
Molinc <92622171+Molinc@users.noreply.github.com>
2024-05-21 15:19:57 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
-document.addEventListener('DOMContentLoaded', async () => {
-  const Server_URL = 'http://localhost:3000';
+document.addEventListener("DOMContentLoaded", async () => {
+  const Server_URL = "http://localhost:3000";
 
   async function serverAddStudent(obj) {
     const response = await fetch(`${Server_URL}/api/students`, {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
       body: JSON.stringify({
         name: obj.name,
         surname: obj.surname,
@@ -20,8 +20,8 @@ document.addEventListener('DOMContentLoaded', async () => {
 
   async function serverGetStudent() {
     const response = await fetch(`${Server_URL}/api/students`, {
-      method: 'GET',
-      headers: { 'Content-Type': 'application/json' },
+      method: "GET",
+      headers: { "Content-Type": "application/json" },
     });
     const data = await response.json();
     return data;
@@ -29,7 +29,9 @@ document.addEventListener('DOMContentLoaded', async () => {
 
   async function serverDeleteStudent(obj) {
     const id = obj;
-    const response = await fetch(`${Server_URL}/api/students/${id}`, { method: 'DELETE' });
+    const response = await fetch(`${Server_URL}/api/students/${id}`, {
+      method: "DELETE",
+    });
     const data = await response.json();
     return data;
   }
@@ -37,55 +39,58 @@ document.addEventListener('DOMContentLoaded', async () => {
   // добавление студента
 
   function initSortListener() {
-    const th = document.querySelectorAll('th');
+    const th = document.querySelectorAll("th");
     const listener = (e) => {
-      const sorttd = (studentsList, prop, dir = false) => studentsList.sort((a, b) => ((!dir ? a[prop] < b[prop] : a[prop] > b[prop]) ? -1 : 1));
+      const sorttd = (studentsList, prop, dir = false) =>
+        studentsList.sort((a, b) =>
+          (!dir ? a[prop] < b[prop] : a[prop] > b[prop]) ? -1 : 1
+        );
 
-      const currentSort = e.target.getAttribute('data-sort');
+      const currentSort = e.target.getAttribute("data-sort");
       if (!currentSort) {
-        e.target.setAttribute('data-sort', 'desc');
-      } else if (currentSort === 'desc') {
-        e.target.setAttribute('data-sort', 'asc');
+        e.target.setAttribute("data-sort", "desc");
+      } else if (currentSort === "desc") {
+        e.target.setAttribute("data-sort", "asc");
       } else {
-        e.target.setAttribute('data-sort', 'desc');
+        e.target.setAttribute("data-sort", "desc");
       }
 
-      const dir = currentSort === 'desc';
+      const dir = currentSort === "desc";
 
-      switch (e.target.getAttribute('data-name')) {
-        case 'fio':
+      switch (e.target.getAttribute("data-name")) {
+        case "fio":
           if (studentsList.name) {
-            studentsList = sorttd(studentsList, 'name', dir);
+            studentsList = sorttd(studentsList, "name", dir);
             console.log(studentsList);
             renderTable(studentsList);
           } else if (studentsList.surname) {
-            studentsList = sorttd(studentsList, 'surname', dir);
+            studentsList = sorttd(studentsList, "surname", dir);
             console.log(studentsList);
             renderTable(studentsList);
           } else {
-            studentsList = sorttd(studentsList, 'lastname', dir);
+            studentsList = sorttd(studentsList, "lastname", dir);
             console.log(studentsList);
             renderTable(studentsList);
           }
-        case 'faculty':
-          studentsList = sorttd(studentsList, 'faculty', dir);
+        case "faculty":
+          studentsList = sorttd(studentsList, "faculty", dir);
           console.log(studentsList);
           renderTable(studentsList);
-        case 'age':
-          studentsList = sorttd(studentsList, 'birthday', dir);
+        case "age":
+          studentsList = sorttd(studentsList, "birthday", dir);
           console.log(studentsList);
           renderTable(studentsList);
-        case 'year-of-study':
-          studentsList = sorttd(studentsList, 'studyStart', dir);
+        case "year-of-study":
+          studentsList = sorttd(studentsList, "studyStart", dir);
           console.log(studentsList);
           renderTable(studentsList);
       }
     };
     th.forEach((th) => {
-      th.removeEventListener('click', listener);
+      th.removeEventListener("click", listener);
     });
     th.forEach((th) => {
-      th.addEventListener('click', listener);
+      th.addEventListener("click", listener);
     });
   }
 
@@ -99,59 +104,59 @@ document.addEventListener('DOMContentLoaded', async () => {
     renderTable(studentsList);
   }
 
-  const form = document.getElementById('add-form');
+  const form = document.getElementById("add-form");
 
   function validateForm() {
     let res = true;
 
     function createTxError(input, text) {
       const cont = input.parentNode;
-      const tx = document.createElement('span');
+      const tx = document.createElement("span");
       tx.textContent = text;
-      tx.classList.add('error');
-      tx.style.color = 'red';
+      tx.classList.add("error");
+      tx.style.color = "red";
       cont.append(tx);
     }
 
     function createTxErrors(select, text) {
       const cont = select.parentNode;
-      const tx = document.createElement('span');
+      const tx = document.createElement("span");
       tx.textContent = text;
-      tx.classList.add('error');
-      tx.style.color = 'red';
+      tx.classList.add("error");
+      tx.style.color = "red";
       cont.append(tx);
     }
 
     function createErrorIn(input) {
-      input.style.border = '1px solid red';
+      input.style.border = "1px solid red";
     }
 
     function createErrorSt(select) {
-      select.style.border = '1px solid red';
+      select.style.border = "1px solid red";
     }
 
     function clearErrorIn(inputs) {
-      inputs.forEach((input) => input.removeAttribute('style'));
+      inputs.forEach((input) => input.removeAttribute("style"));
     }
 
     function clearErrorSt(selects) {
-      selects.forEach((select) => select.removeAttribute('style'));
+      selects.forEach((select) => select.removeAttribute("style"));
     }
 
-    clearErrorIn(form.querySelectorAll('input'));
-    clearErrorSt(form.querySelectorAll('select'));
-    form.querySelectorAll('.error').forEach((item) => item.remove());
+    clearErrorIn(form.querySelectorAll("input"));
+    clearErrorSt(form.querySelectorAll("select"));
+    form.querySelectorAll(".error").forEach((item) => item.remove());
 
-    form.querySelectorAll('select').forEach((select) => {
-      if (select.value === '') {
+    form.querySelectorAll("select").forEach((select) => {
+      if (select.value === "") {
         res = false;
         createErrorSt(select);
-        createTxErrors(select, 'Выберите факультет');
+        createTxErrors(select, "Выберите факультет");
       }
     });
 
-    form.querySelectorAll('input').forEach((input) => {
-      if (input.value === '') {
+    form.querySelectorAll("input").forEach((input) => {
+      if (input.value === "") {
         res = false;
         createErrorIn(input);
       }
@@ -164,58 +169,58 @@ document.addEventListener('DOMContentLoaded', async () => {
           return res;
         }
         createErrorIn(input);
-        createTxError(input, 'Введите корректную дату рождения');
+        createTxError(input, "Введите корректную дату рождения");
         const res = false;
         return res;
       }
 
-      switch (input.getAttribute('id')) {
-        case 'name':
-          if (input.value === '') {
+      switch (input.getAttribute("id")) {
+        case "name":
+          if (input.value === "") {
             res = false;
-            createTxError(input, 'Введите имя');
+            createTxError(input, "Введите имя");
             createErrorIn(input);
           }
           break;
-        case 'surname':
-          if (input.value === '') {
+        case "surname":
+          if (input.value === "") {
             res = false;
-            createTxError(input, 'Введите фамилию');
+            createTxError(input, "Введите фамилию");
             createErrorIn(input);
           }
           break;
-        case 'lastname':
-          if (input.value === '') {
+        case "lastname":
+          if (input.value === "") {
             res = false;
-            createTxError(input, 'Введите отчество');
+            createTxError(input, "Введите отчество");
             createErrorIn(input);
           }
           break;
-        case 'studyStart':
+        case "studyStart":
           const yearNow = new Date().getFullYear();
-          if (input.value === '') {
+          if (input.value === "") {
             res = false;
-            createTxError(input, 'Введите дату поступления');
+            createTxError(input, "Введите дату поступления");
             createErrorIn(input);
           } else if (input.value > yearNow) {
             res = false;
-            createTxError(input, 'Введите корректную дату поступления');
+            createTxError(input, "Введите корректную дату поступления");
             createErrorIn(input);
           }
           break;
-        case 'birthday':
-          if (input.value === '') {
+        case "birthday":
+          if (input.value === "") {
             res = false;
             createErrorIn(input);
-            createTxError(input, 'Введите дату рождения');
+            createTxError(input, "Введите дату рождения");
             return res;
           }
           res = checkBirthDate(input);
       }
     });
 
-    form.querySelectorAll('select').forEach((select) => {
-      if (select.value == '') {
+    form.querySelectorAll("select").forEach((select) => {
+      if (select.value == "") {
         res = false;
         createErrorSt(select);
         return res;
@@ -225,13 +230,13 @@ document.addEventListener('DOMContentLoaded', async () => {
   }
 
   function renderTableTr(obj) {
-    const tr = document.createElement('tr');
-    tr.classList.add('sort');
-    const td1 = document.createElement('td');
-    const td2 = document.createElement('td');
-    const td3 = document.createElement('td');
-    const td4 = document.createElement('td');
-    const td5 = document.createElement('td');
+    const tr = document.createElement("tr");
+    tr.classList.add("sort");
+    const td1 = document.createElement("td");
+    const td2 = document.createElement("td");
+    const td3 = document.createElement("td");
+    const td4 = document.createElement("td");
+    const td5 = document.createElement("td");
 
     td1.textContent = `${obj.surname} ${obj.name} ${obj.lastname}`;
     td2.textContent = `${obj.faculty}`;
@@ -246,13 +251,15 @@ document.addEventListener('DOMContentLoaded', async () => {
       }
       function ageName(age) {
         if (age >= 10 && age <= 20) {
-          return 'лет';
-        } if (age % 10 === 1) {
-          return 'год';
-        } if (age % 10 > 1 && age % 10 < 5) {
-          return 'года';
+          return "лет";
         }
-        return 'лет';
+        if (age % 10 === 1) {
+          return "год";
+        }
+        if (age % 10 > 1 && age % 10 < 5) {
+          return "года";
+        }
+        return "лет";
       }
 
       function formatDate(date) {
@@ -282,11 +289,13 @@ document.addEventListener('DOMContentLoaded', async () => {
       const studyEnd = Number(studyStart) + 4;
       function nameCouse(studyStart, studyEnd) {
         const currentYear = new Date().getFullYear();
-        let course = '';
+        let course = "";
         if (currentYear > studyEnd) {
           course = `${studyStart}-${studyEnd} (Закончил/а)`;
         } else {
-          course = `${studyStart}-${studyEnd} (${currentYear - studyStart} курс)`;
+          course = `${studyStart}-${studyEnd} (${
+            currentYear - studyStart
+          } курс)`;
         }
         return course;
       }
@@ -294,22 +303,21 @@ document.addEventListener('DOMContentLoaded', async () => {
       return strCouse;
     }
 
-    const {studyStart} = obj;
+    const { studyStart } = obj;
 
     td4.textContent = createCouse(studyStart);
 
-    const delBtn = document.createElement('button');
-    delBtn.textContent = 'Удалить';
-    delBtn.classList.add('btn');
-    delBtn.classList.add('btn-outline-danger');
+    const delBtn = document.createElement("button");
+    delBtn.textContent = "Удалить";
+    delBtn.classList.add("btn");
+    delBtn.classList.add("btn-outline-danger");
     td5.append(delBtn);
 
-    delBtn.addEventListener('click', async () => {
-      const id = tr.getAttribute('id');
-      await serverDeleteStudent(id);
-      const studentDel = studentsList.filter((v) => !v.id);
-      console.log(studentDel);
-      renderTable(studentDel);
+    delBtn.addEventListener("click", async () => {
+      const id = tr.getAttribute("id");
+      serverDeleteStudent(id);
+      studentsList = await serverGetStudent();
+      renderTable(studentsList);
     });
 
     tr.append(td1);
@@ -318,15 +326,18 @@ document.addEventListener('DOMContentLoaded', async () => {
     tr.append(td4);
     tr.append(td5);
 
-    const {id} = obj;
-    tr.setAttribute('id', id);
+    const { id } = obj;
+    tr.setAttribute("id", id);
+
+    console.log(tr);
 
     return tr;
   }
 
   function renderTable(array) {
-    const table = document.getElementById('table-body');
-    table.innerHTML = ' ';
+    const table = document.getElementById("table-body");
+    table.innerHTML = "";
+
     array.forEach((obj) => {
       table.append(renderTableTr(obj));
     });
@@ -335,22 +346,22 @@ document.addEventListener('DOMContentLoaded', async () => {
   }
 
   function clearForm() {
-    form.querySelectorAll('input').forEach((input) => input.value = '');
-    form.querySelectorAll('select').forEach((select) => select.value = '');
+    form.querySelectorAll("input").forEach((input) => (input.value = ""));
+    form.querySelectorAll("select").forEach((select) => (select.value = ""));
   }
 
-  document.addEventListener('submit', async (ev) => {
+  document.addEventListener("submit", async (ev) => {
     ev.preventDefault();
     if (validateForm(form)) {
       const i = studentsList.length;
       const student = {
         id: i + 1,
-        name: document.getElementById('name').value,
-        surname: document.getElementById('surname').value,
-        lastname: document.getElementById('lastname').value,
-        studyStart: document.getElementById('studyStart').value,
-        birthday: new Date(document.getElementById('birthday').value),
-        faculty: document.getElementById('faculty').value,
+        name: document.getElementById("name").value,
+        surname: document.getElementById("surname").value,
+        lastname: document.getElementById("lastname").value,
+        studyStart: document.getElementById("studyStart").value,
+        birthday: new Date(document.getElementById("birthday").value),
+        faculty: document.getElementById("faculty").value,
       };
       clearForm();
       const serverAddObj = await serverAddStudent(student);
@@ -361,30 +372,30 @@ document.addEventListener('DOMContentLoaded', async () => {
   });
 
   function filterArray(arr, key, val) {
-    const table = document.getElementById('table-body');
+    const table = document.getElementById("table-body");
     switch (key) {
-      case 'search-fio':
+      case "search-fio":
         return arr.filter((object) => {
           const fio = `${object.surname} ${object.name} ${object.lastname}`;
-          table.innerHTML = '';
+          table.innerHTML = "";
           return fio.includes(val);
         });
-      case 'search-year-of-admission':
+      case "search-year-of-admission":
         return arr.filter((object) => {
           const yearstudyStart = `${object.studyStart}-${object.studyEnd}`;
-          table.innerHTML = '';
+          table.innerHTML = "";
           return yearstudyStart.includes(val);
         });
-      case 'search-year-of-graduation':
+      case "search-year-of-graduation":
         return arr.filter((object) => {
           const yearstudyEnd = String(Number(object.studyStart) + 4);
-          table.innerHTML = '';
+          table.innerHTML = "";
           return yearstudyEnd.includes(val);
         });
-      case 'search-faculty':
+      case "search-faculty":
         return arr.filter((object) => {
           const { faculty } = object;
-          table.innerHTML = '';
+          table.innerHTML = "";
           return faculty.includes(val);
         });
       default:
@@ -392,34 +403,46 @@ document.addEventListener('DOMContentLoaded', async () => {
     }
   }
 
-  document.querySelector('.search').querySelectorAll('.search-input').forEach((input) => {
-    input.addEventListener('keyup', async (e) => {
-      let arraySort = await serverGetStudent();
-      switch (input.getAttribute('id')) {
-        case 'search-fio':
-          arraySort = filterArray(studentsList, 'search-fio', e.target.value);
-          renderTable(arraySort);
-          break;
-        case 'search-year-of-admission'.trim():
-          arraySort = filterArray(studentsList, 'search-year-of-admission', e.target.value);
-          renderTable(arraySort);
-          break;
-        case 'search-year-of-graduation'.trim():
-          arraySort = filterArray(studentsList, 'search-year-of-graduation', e.target.value);
-          renderTable(arraySort);
-          break;
-      }
+  document
+    .querySelector(".search")
+    .querySelectorAll(".search-input")
+    .forEach((input) => {
+      input.addEventListener("keyup", async (e) => {
+        let arraySort = await serverGetStudent();
+        switch (input.getAttribute("id")) {
+          case "search-fio":
+            arraySort = filterArray(studentsList, "search-fio", e.target.value);
+            renderTable(arraySort);
+            break;
+          case "search-year-of-admission".trim():
+            arraySort = filterArray(
+              studentsList,
+              "search-year-of-admission",
+              e.target.value
+            );
+            renderTable(arraySort);
+            break;
+          case "search-year-of-graduation".trim():
+            arraySort = filterArray(
+              studentsList,
+              "search-year-of-graduation",
+              e.target.value
+            );
+            renderTable(arraySort);
+            break;
+        }
+      });
     });
-  });
 
-  document.querySelector('.search').querySelectorAll('select').forEach((select) => {
-    let arraySort = serverGetStudent();
-    select.addEventListener('change', async (e) => {
-      arraySort = filterArray(studentsList, 'search-faculty', e.target.value);
-      renderTable(arraySort);
+  document
+    .querySelector(".search")
+    .querySelectorAll("select")
+    .forEach((select) => {
+      let arraySort = serverGetStudent();
+      select.addEventListener("change", async (e) => {
+        arraySort = filterArray(studentsList, "search-faculty", e.target.value);
+        renderTable(arraySort);
+      });
     });
-  });
-
-  console.log(studentsList);
-
+  
 });
